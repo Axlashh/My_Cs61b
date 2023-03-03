587030ccc779f82fccd64532a876ebd506a1c696
Axlashh <2816141946@qq.com>
2023-03-03 23:56:44 +0800

    s
    delete sth

--- lab6/src/main.java
+++ lab6/src/main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class main {
+public class main {
+    public static void main(String[] args) {
+        DisjointSets a = new DisjointSets(8);
+        a.union(4,8);
+        a.union(5,4);
+        System.out.print(a.connected(5,8));
+    }
 }
--- lab7/src/BSTMap.java
+++
@@ -1,9 +0,0 @@
-public class BSTMap<K, V> implements Map61B<K, V>{
-    private int size;
-    private class Node<K, V> {
-        K key;
-        V value;
-        Node<K, V> left;
-        Node<K, V> right;
-    }
-}
