92dde85654b5ef66f081dc39c24d45d70a7b8f11
Axlashh <2816141946@qq.com>
2023-03-06 12:10:39 +0800

    hw2 finished 1

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,75 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private final int size;
+    private WeightedQuickUnionUF map;
+    private int onum;
+    private boolean[][] omap;
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N must be positive!");
+        }
+        omap = new boolean[N][N];
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                omap[i][j] = false;
+            }
+        }
+        map = new WeightedQuickUnionUF(N * N);
+        onum = 0;
+        size = N;
+    }
+
+    public int pos(int row, int col) {
+        return row * size + col;
+    }
+
+    public void open(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new IndexOutOfBoundsException("Cant open !");
+        }
+        onum += 1;
+        omap[row][col] = true;
+        if (row + 1 < size && isOpen(row + 1, col))
+            map.union(pos(row + 1, col), pos(row, col));
+        if (row - 1 >= 0 && isOpen(row - 1, col))
+            map.union(pos(row - 1, col), pos(row, col));
+        if (col + 1 < size && isOpen(row, col + 1))
+            map.union(pos(row, col + 1), pos(row, col));
+        if (col - 1 >= 0 && isOpen(row, col - 1))
+            map.union(pos(row, col - 1), pos(row, col));
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new IndexOutOfBoundsException("Cant isOpen!");
+        }
+        return omap[row][col];
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new IndexOutOfBoundsException("Cant isFull!");
+        }
+        if (!isOpen(row, col)) return false;
+        for (int i = 0; i < size; i += 1) {
+            if (map.connected(pos(row, col), pos(0, i)))
+                return true;
+        }
+        return false;
+    }
+
+    public int numberOfOpenSites() {
+        return onum;
+    }
+
+    public boolean percolates() {
+        int row = size - 1;
+        for (int i = 0; i < size; i += 1) {
+            if (isFull(row, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,62 @@
 package hw2;
 
+import java.util.Random;
+
 public class PercolationStats {
+
+    private double mean;
+    private double stddev;
+    private double confidenceLow;
+    private double confidenceHigh;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        double[] data = new double[N];
+        for (int i = 0; i < T; i += 1) {
+            Percolation test = pf.make(N);
+            Random r = new Random();
+            while (!test.percolates()) {
+                int row = r.nextInt(N);
+                int col = r.nextInt(N);
+                if (test.isOpen(row, col)) continue;
+                test.open(row, col);
+            }
+            data[i] = (double)test.numberOfOpenSites() / (double)(N * N);
+        }
+        double sum = 0;
+        for (double i : data) {
+            sum += i;
+        }
+        mean = sum / T;
+        double k = 0;
+        for (double i : data) {
+            k += Math.pow((i - mean), 2);
+        }
+        stddev = Math.sqrt((k / (T - 1)));
+        confidenceLow = mean - (1.96 / Math.sqrt(T));
+        confidenceHigh = mean + (1.96 / Math.sqrt(T));
+    }
+
+
+
+    /*sample standard deviation of percolation threshold*/
+    public double stddev() {
+        return stddev;
+    }
+
+    /*sample mean of percolation threshold*/
+    public double mean() {
+        return mean;
+    }
+
+    public double confidenceLow() {
+        return confidenceLow;
+    }
+
+    public double confidenceHigh() {
+        return confidenceHigh;
+    }
 
 }
