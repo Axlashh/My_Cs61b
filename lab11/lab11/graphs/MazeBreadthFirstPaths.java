bf2b364945d16426741023ec12b2d0237fcc9d2c
Axlashh <2816141946@qq.com>
2023-03-08 21:01:23 +0800

    lab11 finished 2

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -24,6 +24,7 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
         q = new LinkedList<>();
         q.add(s);
         distTo[s] = 0;
+        edgeTo[s] = s;
         marked[s] = true;
     }
 
